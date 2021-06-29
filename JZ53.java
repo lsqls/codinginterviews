e8e44d7d3e888038ba9207434bbe28256f8649b1
lbs <764666894@qq.com>
2021-06-29 18:01:56 +0800

    在排序数组中查找数字：插入点

--- JZ53.java
+++ JZ53.java
@@ -1,54 +1,21 @@
 public class JZ53 {
-    //在排序数组中查找数字 :中心扩展
-    public int search(int[] nums, int target) {
-        int len=nums.length;
-
-        if(len==0||target>nums[len-1]||target<nums[0])
-            return 0;
-
-        
-        if(nums[0]==nums[len-1]&&nums[0]==target)
-            return len;
-
-        int index=getTargetIndex(nums, target);
-
-
-        if(index==-1)
-            return 0;
-
-        int left=index,right=index;
-
-
-        while(left-1>=0&&nums[left-1]==target)
-            left--;
-        while(right+1<len&&nums[right+1]==target)
-            right++;
-        return right-left+1;
-        
-
-        
-        
+    //在排序数组中查找数字 :寻找右边界（下一个插入点），target的插入点减去target-1的插入点就是长度
+    public int search(int[] nums, int target) { 
+        return getInsertIndex(nums, target)-getInsertIndex(nums, target-1);          
     }
     
 
-    int getTargetIndex(int[] nums,int target){
+    int getInsertIndex(int[] nums,int target){
         int left=0,right=nums.length-1;
         while(left<=right){
             int mid=left+(right-right)/2;
             if(nums[mid]>target){
                 right=mid-1;
             }
-            else if(nums[mid]<target){
+            else{
                 left=mid+1;
             }
-            else{
-                return mid;
-            }
-
-            
-
         }
-
-        return -1;
+        return left;
     }
 }
