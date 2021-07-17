fe73c5a3fb4076ad82c40d6c90ec99894ecf24ca
lbs <764666894@qq.com>
2021-07-17 20:52:04 +0800

    二进制中的1的个数：n&(n-1)

---
+++ src/JZ15.java
@@ -0,0 +1,17 @@
+package src;
+
+public class JZ15 {
+    // you need to treat n as an unsigned value
+
+
+
+    public int hammingWeight(int n) {
+        int cnt=0;
+
+        while(n!=0){
+            n=n&(n-1);
+            cnt++;
+        }
+        return cnt;
+    }
+}
