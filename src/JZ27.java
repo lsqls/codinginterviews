88446acde26a14861f44a14913250ec746e9ba68
lbs <764666894@qq.com>
2021-07-24 21:01:57 +0800

    二叉树的镜像

---
+++ src/JZ27.java
@@ -0,0 +1,11 @@
+package src;
+
+public class JZ27 {
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null)  return null;
+        TreeNode newLeft=invertTree(root.right),newRigth=invertTree(root.left);
+        root.left=newLeft;
+        root.right=newRigth;
+        return root;
+    }
+}
