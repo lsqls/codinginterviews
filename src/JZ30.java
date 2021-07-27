d9377a4e8e4e45a7d784de27b6a5f2bc1d9f83d6
lbs <764666894@qq.com>
2021-07-27 11:55:49 +0800

    包含最小函数的栈：双栈

---
+++ src/JZ30.java
@@ -0,0 +1,40 @@
+package src;
+
+import java.util.Stack;
+
+
+class JZ30{
+    
+}
+class MinStack{
+    /** initialize your data structure here. */
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+    public MinStack() {
+        stack=new Stack<>();
+        minStack=new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(minStack.isEmpty()||val<=getMin()){
+            minStack.push(val);
+        }
+            
+    }
+    
+    public void pop() {
+        int popVal=stack.pop();
+        if(popVal==getMin()){
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
